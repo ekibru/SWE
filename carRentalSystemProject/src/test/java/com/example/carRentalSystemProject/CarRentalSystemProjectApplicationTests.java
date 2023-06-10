197a35d8e7173b149311d4130bf700bcc11498d7
efulala <kibru4204@gmail.com>
2023-06-10 17:13:22 -0500

    first commit

---
+++ carRentalSystemProject/src/test/java/com/example/carRentalSystemProject/CarRentalSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carRentalSystemProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
