f06e5290aa133d9ec4c3c86bbb5d5ecda241af04
ekibru <kibru4204@gmail.com>
2023-06-12 17:58:37 -0500

    Delete MyStudentMgmtApp/cs425.studentmgmt.model/src/main/java/edu/miu/cs/cs425/studentmgmt directory

--- MyStudentMgmtApp/cs425.studentmgmt.model/src/main/java/edu/miu/cs/cs425/studentmgmt/service/StudentService.java
+++
@@ -1,7 +0,0 @@
-package edu.miu.cs.cs425.studentmgmt.service;
-
-import edu.miu.cs.cs425.studentmgmt.enitity.Student;
-
-public interface StudentService {
-    Student saveStudent(Student student);
-}
